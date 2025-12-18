c6da7916fe232001875af36b60df64d7c30dd294
Asyurazhr <Asyuradjola2004@gmail.com>
2025-12-18 22:16:22 +0700

    add DefaultContainerApp

--- app/src/main/java/com/example/api/repositori/ContainerApp.kt
+++ app/src/main/java/com/example/api/repositori/ContainerApp.kt
@@ -12,3 +12,34 @@ import retrofit2.Retrofit
 interface ContainerApp {
     val repositoryDataSiswa : RepositoryDataSiswa
 }
+class DefaultContainerApp : ContainerApp{
+    private val baseurl = "http://10.0.2.2/umyTI/"
+
+    val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    val klien = OkHttpClient.Builder()
+        .addInterceptor (interceptor = logging)
+        .build()
+
+    private val retrofit : Retrofit = Retrofit.Builder()
+        .baseUrl(baseurl=baseurl)
+        .addConverterFactory(
+            factory = Json {
+                ignoreUnknownKeys = true
+                prettyPrint = true
+                isLenient = true
+            }.asConverterFactory(contentType= "application/json".toMediaType())
+        )
+        .client(client = klien)
+        .build()
+
+    private val retrofitService : ServiceApiSiswa by lazy {
+        retrofit.create(ServiceApiSiswa::class.java)
+    }
+
+    override val repositoryDataSiswa: RepositoryDataSiswa by lazy {
+        JaringanRepositoryDataSiswa(serviceApiSiswa= retrofitService)
+    }
+}
