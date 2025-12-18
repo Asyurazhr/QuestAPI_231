c5fe39cc2ea2937dacbf0d3dec0b169fb122cd19
Asyurazhr <Asyuradjola2004@gmail.com>
2025-12-18 22:22:23 +0700

    add HostNavigasi

--- app/src/main/java/com/example/api/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/api/view/uicontroller/PetaNavigasi.kt
@@ -20,3 +20,19 @@ fun DataSiswaApp(navController: NavHostController = rememberNavController(),
                  modifier: Modifier){
     HostNavigasi(navController = navController)
 }
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavHost(navController = navController, startDestination = DestinasiHome.route,
+        modifier = Modifier ){
+        composable(DestinasiHome.route) {
+            HomeScreen(navigateToItemEntry = { navController.navigate
+                (DestinasiEntry.route) },
+                navigateToItemUpdate = {
+                    navController.navigate("${DestinasiDetail.route}/${it}")
+                }
+            )
+        }
