8d830dba6637062e805e396f26a764893f8cba43
Asyurazhr <Asyuradjola2004@gmail.com>
2025-12-18 22:09:39 +0700

    Add interface ServiceAppSiswa

---
+++ app/src/main/java/com/example/api/apiservice/ServiceApiSiswa.kt
@@ -0,0 +1,10 @@
+package com.example.api.apiservice
+
+import com.example.api.modeldata.DataSiswa
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
+
+interface ServiceApiSiswa {
