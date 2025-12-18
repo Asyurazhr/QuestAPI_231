2b2c646eea66f8e55a8a0ac9ade501afdb81b607
Asyurazhr <Asyuradjola2004@gmail.com>
2025-12-18 22:21:43 +0700

    add DataSiswaApp nav

---
+++ app/src/main/java/com/example/api/view/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,22 @@
+package com.example.api.view.uicontroller
+
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.api.view.EntrySiswaScreen
+import com.example.api.view.HomeScreen
+import com.example.api.view.route.DestinasiEntry
+import com.example.api.view.route.DestinasiHome
+
+
+@Composable
+fun DataSiswaApp(navController: NavHostController = rememberNavController(),
+                 modifier: Modifier){
+    HostNavigasi(navController = navController)
+}
