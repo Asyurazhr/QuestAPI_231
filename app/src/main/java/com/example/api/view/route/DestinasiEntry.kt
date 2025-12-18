4978d84a684b0cc6fb4cc3cfff8a04c5bd768f75
Asyurazhr <Asyuradjola2004@gmail.com>
2025-12-18 22:19:44 +0700

    add DestinasiEntry

---
+++ app/src/main/java/com/example/api/view/route/DestinasiEntry.kt
@@ -0,0 +1,9 @@
+package com.example.api.view.route
+
+import com.example.API.R
+
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
