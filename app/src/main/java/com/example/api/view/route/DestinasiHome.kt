787fe9aaef58d3502c6bc086f06fd40b0541ae34
Asyurazhr <Asyuradjola2004@gmail.com>
2025-12-18 22:20:17 +0700

    add DestinasiHome

---
+++ app/src/main/java/com/example/api/view/route/DestinasiHome.kt
@@ -0,0 +1,9 @@
+package com.example.api.view.route
+
+import com.example.API.R
+
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
