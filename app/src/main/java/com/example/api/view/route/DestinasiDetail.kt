ccb717986bcb7c3af43ab4344e3eb44e95bb6e94
Asyurazhr <Asyuradjola2004@gmail.com>
2025-12-26 12:35:30 +0700

    Fix detail route args

--- app/src/main/java/com/example/api/view/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/api/view/route/DestinasiDetail.kt
@@ -6,3 +6,6 @@ object DestinasiDetail : DestinasiNavigasi {
     override val route: String = "detail_siswa"
     override val titleRes: Int = R.string.detail_siswa
 
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs: String = "$route/{$itemIdArg}"
+}
