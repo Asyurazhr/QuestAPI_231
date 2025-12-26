9f541446aa6a20885539f0e9d35a9692319daa17
Asyurazhr <Asyuradjola2004@gmail.com>
2025-12-26 12:34:26 +0700

    add string

---
+++ app/src/main/java/com/example/api/view/route/DestinasiDetail.kt
@@ -0,0 +1,8 @@
+package com.example.api.view.route
+
+import com.example.api.R
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route: String = "detail_siswa"
+    override val titleRes: Int = R.string.detail_siswa
+
